93569e4316f4d0079978fe7637b5bab2dafd583c
ducdetronquito <g.paulet@zero-gachis.com>
2021-08-25 06:38:17 +0200

    📦 Packaged for Gyro

--- build.zig
+++ build.zig
@@ -1,21 +1,15 @@
 const Builder = @import("std").build.Builder;
-const packages = @import("lib/packages.zig");
+const pkgs = @import("deps.zig").pkgs;
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const lib = b.addStaticLibrary("requestz", "src/main.zig");
-    lib.addPackage(packages.network);
-    lib.addPackage(packages.iguanaTLS);
-    lib.addPackage(packages.h11);
-    lib.addPackage(packages.http);
+    pkgs.addAllTo(lib);
     lib.setBuildMode(mode);
     lib.install();
 
     var main_tests = b.addTest("src/tests.zig");
-    main_tests.addPackage(packages.network);
-    main_tests.addPackage(packages.iguanaTLS);
-    main_tests.addPackage(packages.h11);
-    main_tests.addPackage(packages.http);
+    pkgs.addAllTo(main_tests);
     main_tests.setBuildMode(mode);
 
     const test_step = b.step("test", "Run library tests");
